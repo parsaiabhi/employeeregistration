9a17b853449da889c5f7902a0fc7571db724cee4
abhishek <tech.parsai@gmail.com>
2019-10-07 23:17:23 +0530

    initial commit

---
+++ employeeregistration/src/test/java/com/example/employeeregistration/EmployeeregistrationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.employeeregistration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmployeeregistrationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
