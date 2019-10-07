9da389fdae8e2426ac9fc4d3ee8abd64eda9044a
abhishek <tech.parsai@gmail.com>
2019-10-07 23:26:24 +0530

    controller and service committed

---
+++ employeeregistration/src/main/java/com/example/employeeregistration/repository/EmployeeRepository.java
@@ -0,0 +1,20 @@
+package com.example.employeeregistration.repository;
+/*
+ * Copyright (C) 2007-2010 GoMobile Corp.
+ * Copyright (C) 2011, 2013-2016 The GoMobile Team.
+ *
+ */
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.employeeregistration.entity.Employee;
+
+/**
+ *  @author abhishek
+ *
+ */
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
+
+}
+
---
+++ employeeregistration/src/main/java/com/example/employeeregistration/service/EmployeeServiceImpl.java
@@ -0,0 +1,16 @@
+package com.example.employeeregistration.service;
+/*
+ * Copyright (C) 2007-2010 GoMobile Corp.
+ * Copyright (C) 2011, 2013-2016 The GoMobile Team.
+ *
+ */
+
+/**
+ *  @author abhishek
+ *
+ */
+
+public class EmployeeServiceImpl {
+
+}
+
